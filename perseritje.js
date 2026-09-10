e03a3c6fe9c95ecfca9a94354134ed7a99628bfc
Albi <albainianchannel@gmail.com>
2026-09-10 17:01:11 +0200

    update

---
+++ perseritje.js
@@ -0,0 +1,15 @@
+
+
+var jcoders = true; //bolean
+var age = 11; //number
+var name = "albi" //string
+
+console.log(jcoders)
+console.log(age)
+console.log(name)
+
+//pjesa 2
+let number1 = 10
+let number2 = 20
+
+console.log("Shuma eshte:" + (number1 + number2))
