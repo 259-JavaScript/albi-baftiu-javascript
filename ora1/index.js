a89af970c4e575510def8acb37b3db6317bdaec3
Albi <albainianchannel@gmail.com>
2026-09-22 17:07:01 +0200

    update

---
+++ ora1/index.js
@@ -0,0 +1,25 @@
+function identiteti(emri ,mbiemri, mosha ){
+    console.log ("une jam + emri + mbiemri + "dhe jam" + mosha +"vjear")
+}
+identiteti( "enar, mehmeti, 14")
+
+
+function mbledhjeR(a,b){
+let rezultati = a + b;
+return rezultatin;
+}
+
+function prodhimi)(a,b,c) {
+ let prodhimi=*a*b*c;
+return prodhimi
+}
+
+let shuma = prodhimi(10,10,10)
+ console.log(shuma)
+
+// arrow function
+
+const mbledharrow  = *(a,b)=>}{a+b}
+
+
+ const prodhimdharrow = (4,6)
